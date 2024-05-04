d695f7432b390d1bf58d692d0b2dc2667c24c603
Anthony Fasano <a.fasano@kralys.ch>
2024-05-04 16:19:58 +0200

    Date time mission Fixes #3

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -5,6 +5,8 @@ import { AppCacheProvider } from '@mui/material-nextjs/v14-pagesRouter'
 import { ThemeProvider } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import theme from '../lib/theme'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props
@@ -13,10 +15,12 @@ export default function MyApp(props: AppProps) {
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
+      </LocalizationProvider>
     </AppCacheProvider>
   )
 }
