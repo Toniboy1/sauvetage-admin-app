e25525734c40fdcbe3334380344fb4fc805a30bf
Anthony Fasano <a.fasano@kralys.ch>
2024-05-13 03:14:31 +0200

    moved drawer if half screen

--- renderer/components/appbar/index.tsx
+++ renderer/components/appbar/index.tsx
@@ -1,48 +1,103 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
-import Data from "../data";
-import { useUpdate } from "../providers/update";
-import { pages } from "../../site";
-
-
+import React, { useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Button from '@mui/material/Button';
+import Data from '../data';
+import { useUpdate } from '../providers/update';
+import { pages } from '../../site';
 
 const AppNavBar = () => {
   const { updateMessage } = useUpdate();
-  console.log(updateMessage);
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <List>
+        {pages.map((page) => (
+          <ListItem key={page.href} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }} onClick={() => { window.location.href = page.href; }}>
+              <ListItemText primary={page.name} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
   return (
-    <AppBar position="sticky"sx={{
-      '-webkit-app-region': 'drag',
-      '-webkit-user-select': 'none',
-      cursor: 'default'}}>
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            {pages.map((page) => (
-              <Button
-                key={page.href}
-                onClick={() => {
-                  window.location.href = page.href;
-                }}
-                sx={{ my: 2, color: "white", display: "block" }}
-              >
-                {page.name}
-              </Button>
-            ))}
-            {updateMessage && (
-              <Typography sx={{ mt: 4, mb: 2, color: "secondary.main" }}>
-                {updateMessage}
-              </Typography>
-            )}
-            <Data />
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+    <Box sx={{ display: 'flex' }}>
+      <AppBar position="sticky" sx={{
+        '-webkit-app-region': 'drag',
+        '-webkit-user-select': 'none',
+        cursor: 'default'
+      }}>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{
+                mr: 2,
+                display: { sm: 'none' },
+                '-webkit-app-region': 'no-drag'  // Ensure the icon button is clickable
+              }}
+              onClick={handleDrawerToggle}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Box sx={{ flexGrow: 1, display: { xs: 'none', sm: 'flex' } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page.href}
+                  onClick={() => { window.location.href = page.href; }}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {page.name}
+                </Button>
+              ))}
+              {updateMessage && (
+                <Typography sx={{ mt: 4, mb: 2, color: 'secondary.main' }}>
+                  {updateMessage}
+                </Typography>
+              )}
+              <Data />
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <Drawer
+        variant="temporary"
+        color='primary'
+        open={mobileOpen}
+        onClose={handleDrawerToggle}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 240 },
+        }}
+      >
+        {drawer}
+      </Drawer>
+    </Box>
   );
 };
+
 export default AppNavBar;
